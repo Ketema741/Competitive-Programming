266fcf4a3d171b64fb6c872fce54049260c6698a
ketema741 <ketemagirma15@gmail.com>
2022-10-02 09:13:45 -0700

    insertion sort part one

---
+++ week1/insertionSort1.py
@@ -0,0 +1,11 @@
+def insertionSort1(n, arr):
+    unsorted = arr[n - 1]
+    i = n - 1
+    while i > 0 and arr[i - 1] > unsorted:
+        arr[i] = arr[i - 1]
+        print(*arr)
+        i -= 1
+    arr[i] = unsorted
+    print(*arr)
+
+insertionSort1(5, [2, 6, 4, 8, 3])
