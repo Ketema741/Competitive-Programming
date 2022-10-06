ef981a58ed77cf7e9ab3deea04afa430acc6a2dd
ketema741 <ketemagirma15@gmail.com>
2022-10-06 07:08:02 +0300

    counting valley

---
+++ week1/countingValleys.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    altitude, valley_count = 0, 0
+    for x in path:
+        if x == "D":
+            altitude -= 1
+        elif x=="U":
+            if altitude == -1:
+                valley_count +=1
+            altitude += 1
+    return valley_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
