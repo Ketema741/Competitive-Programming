2bceb1289e7f185dbc23a7e8601f58f1751b4aff
ketema741 <ketemagirma15@gmail.com>
2022-10-08 11:23:09 +0300

    Valid Parentheses

---
+++ week2/isValid.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        flag = True
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "{" or s[i] == "[":
+                stk.append(s[i])
+            elif s[i] == ")" or s[i] == "}" or s[i] == "]":
+                if len(stk) == 0:
+                    flag = False
+                    break
+                else:
+                    if s[i] == ")" and stk[-1] == "(":
+                        stk.pop()
+                    elif s[i] == "}" and stk[-1] == "{":
+                        stk.pop()
+                    elif s[i] == "]" and stk[-1] == "[":
+                        stk.pop()
+                    else:
+                        flag = False
+                        break
+        return flag if len(stk) == 0 else False
