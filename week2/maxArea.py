37a28d51d95faaedbbf8793a10ad877a8d0e1c14
ketema741 <ketemagirma15@gmail.com>
2022-10-11 17:21:50 +0300

    max area using two pointer

---
+++ week2/maxArea.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height)-1
+        res = 0
+        while r > l :
+            area = (r-l) * min(height[r], height[l])
+            res = max(res, area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
