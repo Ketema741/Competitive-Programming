1b95445020047c3f65513fcc38a4a770fb01084c
ketema741 <ketemagirma15@gmail.com>
2022-10-13 07:46:01 +0300

    move zeros two pointer

--- week2/moveZeroes.py
+++ week2/moveZeroes.py
@@ -1,17 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        l = r =0
-        while r !=len(nums)-1:
-            if nums[r] != 0:
-                r += 1
-                l += 1
-            while r != len(nums)-1 and nums[r] == 0:
-                r += 1
-            if l < r and nums[l] == 0:
-                tmp = nums[r]
-                nums[r] = nums[l]
-                nums[l] = tmp
-                l += 1
+        slow  = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            if nums[slow] != 0:
+                slow += 1
                 
                 
                 
