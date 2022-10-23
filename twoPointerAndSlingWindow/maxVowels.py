32a00943d098203e9cd39443e30ccf8a30c5656a
ketema741 <ketemagirma15@gmail.com>
2022-10-23 16:06:35 +0300

    max vowels two pointers

---
+++ twoPointerAndSlingWindow/maxVowels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        left = res = vCount = 0
+        v = 'aeuio'
+        for right in range(len(s)):
+            if s[right] in v:
+                vCount += 1
+            if (right - left + 1) == k:
+                res = max(res, vCount)
+                if s[left] in v:
+                    vCount -= 1
+                left += 1
+        return res
